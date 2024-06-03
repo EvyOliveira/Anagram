082c7b5cedc6095a9880b55e501580ec08c3d704
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-03 16:13:47 -0300

    Add anagram validation algorithm

---
+++ ValidAnagram/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func isAnagram(firstString string, secoundString string) bool {
+	charactersMap := make(map[rune]int)
+	for _, character := range firstString {
+		charactersMap[character]++
+	}
+
+	for _, character := range secoundString {
+		if count, ok := charactersMap[character]; ok {
+			count--
+			if count == 0 {
+				delete(charactersMap, character)
+			} else {
+				charactersMap[character] = count
+			}
+		} else {
+			return false
+		}
+	}
+	return len(charactersMap) == 0
+}
+
+func main() {
+	firstWord := "anagram"
+	secoundWord := "nagaram"
+
+	firstAndSecoundComparison := isAnagram(firstWord, secoundWord)
+	fmt.Printf("First word is %s and secound word is %s. They are anagrams? %v\n", firstWord, secoundWord, firstAndSecoundComparison)
+
+	thirdWord := "rat"
+	fourthWord := "car"
+	thirdAndFourthComparison := isAnagram(thirdWord, fourthWord)
+	fmt.Printf("Third word is %s and fourth word is %s. They are anagrams? %v\n", thirdWord, fourthWord, thirdAndFourthComparison)
+
+}
