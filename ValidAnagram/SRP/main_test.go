ff5083a05bbc1c209c0c460ab99efba3f812e207
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-04 12:17:52 -0300

    Add improvements for single responsibility principle

--- ValidAnagram/SRP/main_test.go
+++ ValidAnagram/SRP/main_test.go
@@ -3,8 +3,6 @@ package main
 import "testing"
 
 func TestAnagramChecker_IsAnagram(t *testing.T) {
-	checker := &AnagramChecker{}
-
 	tests := []struct {
 		name          string
 		firstString   string
@@ -13,13 +11,17 @@ func TestAnagramChecker_IsAnagram(t *testing.T) {
 	}{
 		{name: "it's an anagram", firstString: "anagram", secoundString: "nagaram", expected: true},
 		{name: "it's not an anagram", firstString: "rat", secoundString: "car", expected: false},
+		{name: "empty string", firstString: "", secoundString: "test", expected: false},
+		{name: "both empty strings", firstString: "", secoundString: "", expected: false},
+		{name: "different case", firstString: "Race", secoundString: "care", expected: true},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			current := checker.IsAnagram(test.firstString, test.secoundString)
+			anagramChecker := AnagramChecker{}
+			current := anagramChecker.IsAnagram(test.firstString, test.secoundString)
 			if current != test.expected {
-				t.Errorf("Expected %v, got %v", test.expected, current)
+				t.Errorf("Test case '%s': expected %v, got %v", test.name, test.expected, current)
 			}
 		})
 	}
