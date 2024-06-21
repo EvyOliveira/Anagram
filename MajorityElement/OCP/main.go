ff58f54a8618bcae87493bdb0709d0e04432ded5
evyoliveira <cristinioliveira.eo@gmail.com>
2024-06-21 17:19:08 -0300

    Fix logic to consider the highest value between numbers with the same frequency

--- MajorityElement/OCP/main.go
+++ MajorityElement/OCP/main.go
@@ -3,79 +3,72 @@ package main
 import (
 	"errors"
 	"fmt"
-	"sort"
 )
 
 type MajorityChecker interface {
 	FindMajorityElement(num []int64) (int64, error)
 }
 
-type HashTableMajorityElement struct{}
+type MajorityElement struct{}
 
-func (h *HashTableMajorityElement) FindMajorityElement(nums []int64) (int64, error) {
-	if len(nums) == 0 {
-		return 0, errors.New("slice is empty")
-	}
-
-	elements := make(map[int64]int64)
-	for _, num := range nums {
-		elements[num]++
-		if elements[num] > int64(len(nums)/2) {
-			return num, nil
-		}
-	}
-	return -1, errors.New("majority element not found")
-}
-
-type SortingMajorityElement struct{}
-
-func (s *SortingMajorityElement) FindMajorityElement(nums []int64) (int64, error) {
-	if len(nums) == 0 {
-		return 0, errors.New("slice is empty")
-	}
-
-	sortedNums := make([]int64, len(nums))
-	copy(sortedNums, nums)
-	sort.Slice(sortedNums, func(i, j int) bool {
-		return sortedNums[i] < sortedNums[j]
-	})
-
-	mid := int64(len(sortedNums) / 2)
+func (h *MajorityElement) FindMajorityElement(nums []int64) (int64, error) {
+	candidate := nums[0]
 	count := 1
 
-	for i := mid + 1; i < int64(len(sortedNums)); i++ {
-		if sortedNums[i] == sortedNums[mid] {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == candidate {
 			count++
+		} else {
+			count--
+			if count == 0 {
+				candidate = nums[i]
+				count = 1
+			}
 		}
 	}
-	if count > len(nums)/2 {
-		return sortedNums[mid], nil
-	}
-
-	return sortedNums[mid], nil
+	return candidate, nil
 }
 
-func isAnEmptySlice(finder MajorityChecker, nums []int64) (int64, error) {
-	return finder.FindMajorityElement(nums)
+func findMajorityElement(checker MajorityChecker, nums []int64) (int64, error) {
+	if len(nums) == 0 {
+		return 0, errors.New("slice is empty")
+	}
+	return checker.FindMajorityElement(nums)
+}
+
+func isAnEmptySlice(nums []int64) (int64, error) {
+	if len(nums) == 0 {
+		return 0, errors.New("slice is empty")
+	}
+	return 1, nil
 }
 
 func main() {
 	nums1 := []int64{2, 40, 300, 40, 1000, 300}
-	nums2 := []int64{1000, 2, 300, 40, 1000, 1000}
+	nums2 := []int64{1000, 2, 300, 300, 300, 40, 1000, 1000}
 
-	finder := &SortingMajorityElement{}
-
-	result1, err := isAnEmptySlice(finder, nums1)
-	if err != nil {
-		fmt.Println("error for nums1:", err)
-	} else {
-		fmt.Println("majority element in:", nums1, ":", result1)
+	checker := &MajorityElement{}
+	_, firstSliceError := isAnEmptySlice(nums1)
+	if firstSliceError != nil {
+		fmt.Println("error checking nums1:", firstSliceError)
 	}
 
-	result2, err := isAnEmptySlice(finder, nums2)
+	_, secoundSliceError := isAnEmptySlice(nums2)
+	if secoundSliceError != nil {
+		fmt.Println("error checking nums2:", secoundSliceError)
+	}
+
+	majorityElement1, err := findMajorityElement(checker, nums1)
 	if err != nil {
-		fmt.Println("error for nums2:", err)
+		fmt.Println("error finding majority element in nums1:", err)
 	} else {
-		fmt.Println("majority element in:", nums2, ":", result2)
+		fmt.Println("majority element in nums1:", majorityElement1)
+	}
+
+	majorityElement2, err := findMajorityElement(checker, nums2)
+	if err != nil {
+		fmt.Println("error finding majority element in nums2:", err)
+	} else {
+		fmt.Println("majority element in nums2:", majorityElement2)
 	}
 }
