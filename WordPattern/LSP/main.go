890e130241663878a77d02851a386b919d92e095
evyoliveira <cristinioliveira.eo@gmail.com>
2024-08-08 20:59:45 -0300

    Feat split functions in differents contexts

--- WordPattern/LSP/main.go
+++ WordPattern/LSP/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
+	"unicode"
 )
 
 type WordProcessor interface {
@@ -13,6 +15,41 @@ type DefaultWordProcessor struct{}
 
 func (d *DefaultWordProcessor) Split(s string) []string {
 	return strings.Fields(s)
+}
+
+type CustomDelimiterWordProcessor struct {
+	delimiter string
+}
+
+func (c *CustomDelimiterWordProcessor) Split(s string) []string {
+	return strings.Split(s, c.delimiter)
+}
+
+type RegexWordProcessor struct {
+	regex *regexp.Regexp
+}
+
+func (r *RegexWordProcessor) Split(s string) []string {
+	return r.regex.FindAllString(s, -1)
+}
+
+type AdvancedWordProcessor struct{}
+
+func (a *AdvancedWordProcessor) Split(s string) []string {
+	var words []string
+	var word []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			word = append(word, r)
+		} else if len(word) > 0 {
+			words = append(words, string(word))
+			word = nil
+		}
+	}
+	if len(word) > 0 {
+		words = append(words, string(word))
+	}
+	return words
 }
 
 func WordPattern(pattern, s string, wordProcessor WordProcessor) bool {
@@ -44,10 +81,10 @@ func WordPattern(pattern, s string, wordProcessor WordProcessor) bool {
 
 func main() {
 	s := "dog cat cat dog"
-	pattern := "abba"
-	wordProcessor := DefaultWordProcessor{}
+	pattern := "aaaa"
+	defaultProcessor := &DefaultWordProcessor{}
 
-	if WordPattern(pattern, s, &wordProcessor) {
+	if WordPattern(pattern, s, defaultProcessor) {
 		fmt.Println("the string " + s + " follows the pattern " + pattern + ".")
 	} else {
 		fmt.Println("the string " + s + " does not follow the pattern " + pattern + ".")
